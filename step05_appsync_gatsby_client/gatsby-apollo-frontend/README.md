8db49f2f4697ffc5312ba6b1dd33e4ca7c4d03ea
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-11-14 16:37:12 +0500

    Completed Gatsby Apollo Frontend
    Completed Gatsby Apollo Frontend

--- step05_appsync_gatsby_client/gatsby-apollo-frontend/README.md
+++ step05_appsync_gatsby_client/gatsby-apollo-frontend/README.md
@@ -6,3 +6,100 @@
 # Initializing Gatsby Project using starter hello-world
 gatsby new gatsby-apollo-frontend https://github.com/gatsbyjs/gatsby-starter-hello-world
 ```
+
+2. Install Plugin for `@apollo/client`, [Read Docs](https://www.gatsbyjs.com/plugins/gatsby-plugin-apollo/)
+
+```powershell
+npm install gatsby-plugin-apollo @apollo/client
+```
+
+3. Add the plugin to your Gatsby config `gatsby-config.js`, and supply the location of your desired GraphQL API to the `uri` option
+
+```js
+// gatsby-config.js
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-plugin-apollo",
+      options: {
+        uri: process.env.GATSBY_APPSYNC_GRAPHQL_API_URL,
+        headers: {
+          "x-api-key": process.env.APPSYNC_GRAPHQL_API_KEY,
+        },
+      },
+    },
+  ],
+}
+```
+
+4. Use like shown below
+
+- For type `Query` queries
+
+```js
+import React from "react"
+import { useQuery, gql } from "@apollo/client"
+
+const QUERY_WELCOME = gql`
+  query MyQuery {
+    welcome
+  }
+`
+
+const Welcome = () => {
+  const { loading, error, data } = useQuery(QUERY_WELCOME)
+
+  return (
+    <p className="welcome">
+      {loading
+        ? "Loading..."
+        : error
+        ? "Error Occured..."
+        : data && data.welcome}
+    </p>
+  )
+}
+
+export default Welcome
+```
+
+- For type `Mutation` queries
+
+```js
+import React from "react"
+import { useMutation, gql } from "@apollo/client"
+
+const MUTATION_ADD_PRODUCT = gql`
+  mutation MyMutation($newProduct: AddProductInput) {
+    addProduct(product: $newProduct) {
+      id
+      price
+      title
+    }
+  }
+`
+
+const Component = () => {
+    const [myMutateFuncAddProduct, { loading, error, data }] = useQuery(MUTATION_ADD_PRODUCT);
+
+    const addProduct = asycn (product) => {
+        const addedProduct = await myMutateFuncAddProduct({
+            variables: {
+                newProduct: product
+            }
+        })
+
+        return addedProduct;
+    }
+
+    return (
+        // ...
+    )
+}
+
+export default Component;
+```
+
+# Extras
+
+- Environment Variables using `dotenv`, [Read about dotenv inside Gatsby Docs for more details](https://www.gatsbyjs.com/docs/how-to/local-development/environment-variables/)
--- step05_appsync_gatsby_client/gatsby-apollo-frontend/src/components/Products.js
+++ step05_appsync_gatsby_client/gatsby-apollo-frontend/src/components/Products.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../queries';
+import React, { useState, useRef } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
+import { QUERY_PRODUCTS, MUTATION_ADD_PRODUCT, MUTATION_UPDATE_PRODUCT, MUTATION_DELETE_PRODUCT } from '../queries';
 
 const Products = () => {
+    const [currentProduct, setCurrentProduct] = useState(null);
 
-    const { loading, error, data } = useQuery(QUERY_PRODUCTS);
+    const title = useRef('');
+    const price = useRef('');
+    
+    const { loading : productsLoading, error : productsError, data : productsData} = useQuery(QUERY_PRODUCTS);
+    const [ addProduct ] = useMutation(MUTATION_ADD_PRODUCT);
+    const [ updateProduct ] = useMutation(MUTATION_UPDATE_PRODUCT);
+    const [ deleteProduct ] = useMutation(MUTATION_DELETE_PRODUCT);
 
-    if(loading) {
+    if(productsLoading) {
         return <p>Loading Products...</p>
+    } else {
+        if(productsError){
+            return <p>Error occured while loading Products</p>
+        }
     }
 
-    if(error) {
-        return <p>Error Occured in getting Products...</p>
+    const addFormProduct = async e => {
+        e.preventDefault();
+
+        if(title.current.value.trim() !== '' && price.current.value.trim() !== ''){
+            if(currentProduct) {
+                await updateProduct({
+                    variables: {
+                        renewProduct: {
+                            id: currentProduct.id,
+                            title: title.current.value,
+                            price: price.current.value
+                        }
+                    }
+                })
+                title.current.value = '';
+                price.current.value = '';
+
+                console.log('Product has been Updated');
+                setCurrentProduct(null)
+            } else {
+                await addProduct({
+                    variables: {
+                        newProduct: {
+                            title: title.current.value,
+                            price: price.current.value
+                        }
+                    }
+                })    
+                title.current.value = '';
+                price.current.value = '';
+
+                console.log('Product has been Added');
+            }
+        } else {
+            alert('Some credentials are missing.')
+        }
+    }
+
+    const setProductToUpdate = product => {
+        setCurrentProduct(product);
+        title.current.value = product.title;
+        price.current.value = product.price;
     }
 
     return (
@@ -20,28 +71,36 @@ const Products = () => {
             <small><span>Title</span><span>Price</span></small>
             <ul>
                 {
-                    data.products && data.products.length !== 0 ?
-                    data.products.map(({id, title, price}) => (
+                    productsData.products && productsData.products.length !== 0 ?
+                    productsData.products.map(({id, title, price}) => (
                         <li key={id}>
                             <div className="data">
                                 <span>{title}</span>
                                 <span>${price}</span>
                             </div>
                             <div className="controls">
-                                <button className="update">Update</button>
-                                <button className="delete">Delete</button>
+                                <button className="update" onClick={() => setProductToUpdate({id, title, price})}>Update</button>
+                                <button className="delete" onClick={async () => {
+                                    await deleteProduct({
+                                        variables: {
+                                            pid: id
+                                        }
+                                    })
+
+                                    console.log('Product has been Deleted');
+                                }}>Delete</button>
                             </div>
                         </li>
                     )) : <p style={{padding: '10px'}}>No Products available</p>
                 }
             </ul>
-            <form>
-                <h4>Add New Product</h4>
+            <form onSubmit={addFormProduct}>
+                <h4>{currentProduct ? 'Update the Product' : 'Add New Product'}</h4>
                 <div className="inputs">
-                    <input type="text" placeholder="Enter Title" />
-                    <input type="number" placeholder="Enter Price" />
+                    <input ref={title} type="text" placeholder="Enter Title" />
+                    <input ref={price} type="number" placeholder="Enter Price" />
                 </div>
-                <input type="submit" value="Add" />
+                <input type="submit" value={currentProduct ? 'Update' : 'Add'} />
             </form>
         </div>
     )
