948cf3666b896f09bf370bb086138205bd21f2b6
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-11-21 23:19:23 +0500

    Initialized Step08 with cdk init command
    Initialized Step08 with cdk init command

---
+++ step08_appsync_dynamodb_as_datasource_mappingtemplete_methods/test/step08_appsync_dynamodb_as_datasource_mappingtemplete_methods.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Step08AppsyncDynamodbAsDatasourceMappingtempleteMethods from '../lib/step08_appsync_dynamodb_as_datasource_mappingtemplete_methods-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step08_appsync_dynamodb_as_datasource_mappingtemplete_methods-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step08AppsyncDynamodbAsDatasourceMappingtempleteMethods.Step08AppsyncDynamodbAsDatasourceMappingtempleteMethodsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
