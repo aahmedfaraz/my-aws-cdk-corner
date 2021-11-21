af8c373b07431e3d40d61580ea85d923f8c94d51
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-11-22 01:08:39 +0500

    Initialized - Step09 - CDK app usjng cdk init command
    Initialized - Step09 - CDK app usjng cdk init command

---
+++ step09_appsync_dynamodb_as_datasource_using_vtl/test/step09_appsync_dynamodb_as_datasource_using_vtl.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Step09AppsyncDynamodbAsDatasourceUsingVtl from '../lib/step09_appsync_dynamodb_as_datasource_using_vtl-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step09_appsync_dynamodb_as_datasource_using_vtl-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step09AppsyncDynamodbAsDatasourceUsingVtl.Step09AppsyncDynamodbAsDatasourceUsingVtlStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
