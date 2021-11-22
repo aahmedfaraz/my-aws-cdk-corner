37f72a7e0186872ab5981ed4cc735525bb125eb7
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-11-23 02:41:03 +0500

    Completed Backend - Step11 - Example02 - CDK Cognito Userpool Backend
    Completed Backend - Step11 - Example02 - CDK Cognito Userpool Backend

--- step11_cognito/example02_cognito_userpool_with_frontend_amplify/cdk_cognito-backend/lib/cdk_cognito-backend-stack.ts
+++ step11_cognito/example02_cognito_userpool_with_frontend_amplify/cdk_cognito-backend/lib/cdk_cognito-backend-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as cognito from '@aws-cdk/aws-cognito';
 
 export class CdkCognitoBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -7,9 +7,43 @@ export class CdkCognitoBackendStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCognitoBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const userPool = new cognito.UserPool(this, 'MyUserPoolForAmplify', {
+      selfSignUpEnabled: true, // Allows users to signup
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY, // Account Recovery Email
+      userVerification: {
+        // email verification
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+      },
+      autoVerify: {
+        // auto email verification
+        email: true,
+      },
+      standardAttributes: {
+        // attributes required for signup
+        email: {
+          required: true,
+          mutable: true,
+        },
+        phoneNumber: {
+          required: true,
+          mutable: true
+        }
+      }
+    })
+
+    // UserPoolClient to connect your UserPool with your Frontend or any other Third party Identity Provider
+    const userPoolClient = new cognito.UserPoolClient(this, 'UserPoolClientForAmplify', {  
+      userPool,
+    })
+    
+    // We would be needing below mentioned IDs on out CLIENT SIDE
+    // Output UserPool ID
+    new cdk.CfnOutput(this, 'UserPoolId', {
+      value: userPool.userPoolId
+    })
+    // Output UserPoolClient ID
+    new cdk.CfnOutput(this, 'UserPoolClientId', {
+      value: userPoolClient.userPoolClientId
+    })
   }
 }
