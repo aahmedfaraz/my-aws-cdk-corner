f764983fbae8da3907ca9482f23ea3ee74db9488
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-11-23 00:49:32 +0500

    Initialized - Step11 - Example01 - CDK with Cognito - Userpool with Lambda Triggers
    Initialized - Step11 - Example01 - CDK with Cognito - Userpool with Lambda Triggers

---
+++ step11_cognito/example01_userpool_with_lambda_triggers/test/example01_userpool_with_lambda_triggers.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Example01UserpoolWithLambdaTriggers from '../lib/example01_userpool_with_lambda_triggers-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/example01_userpool_with_lambda_triggers-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Example01UserpoolWithLambdaTriggers.Example01UserpoolWithLambdaTriggersStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
