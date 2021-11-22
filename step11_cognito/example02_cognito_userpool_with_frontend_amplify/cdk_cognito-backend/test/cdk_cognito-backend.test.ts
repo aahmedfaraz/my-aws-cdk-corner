5f85041f4824a93b82e8c9e43fe6ba8194cee2fc
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-11-23 01:18:58 +0500

    Initialized Backend - Step11 - Example02 - CDK Cognito Userpool Backend
    Initialized Backend - Step11 - Example02 - CDK Cognito Userpool Backend

---
+++ step11_cognito/example02_cognito_userpool_with_frontend_amplify/cdk_cognito-backend/test/cdk_cognito-backend.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as CdkCognitoBackend from '../lib/cdk_cognito-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_cognito-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCognitoBackend.CdkCognitoBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
