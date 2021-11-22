0c964e64d48ccfcd5a4c90b05d46bd7f3d7b37b3
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-11-22 21:22:15 +0500

    Done - CDK - AppSync with no Datasource
    Done - CDK - AppSync with no Datasource

--- step10_appsync_no_datasource/lib/step10_appsync_no_datasource-stack.ts
+++ step10_appsync_no_datasource/lib/step10_appsync_no_datasource-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as appsync from '@aws-cdk/aws-appsync';
 
 export class Step10AppsyncNoDatasourceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -7,9 +7,53 @@ export class Step10AppsyncNoDatasourceStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Step10AppsyncNoDatasourceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const api = new appsync.GraphqlApi(this, 'apiWithNoDatasource', {
+      name: 'API with no Datasource',
+      schema: appsync.Schema.fromAsset('graphql/schema.gql')
+    });
+
+    const datasource = api.addNoneDataSource('noDatasource', {
+      name: 'NoDatasource',
+      description: 'It does not save the data anywhere just performs operations on data and sends'
+    })
+
+    datasource.createResolver({
+      typeName: 'Query',
+      fieldName: 'readStatus',
+      requestMappingTemplate: appsync.MappingTemplate.fromString(`
+        {
+          "version" : "2017-02-28",
+          "payload" : $util.toJson({
+            "status" : "This is current status"
+          })
+        }
+      `),
+      responseMappingTemplate: appsync.MappingTemplate.fromString(`
+        #if($context.error)
+          $util.error($context.error.message, $context.error.type)
+        #else
+          $util.toJson($context.result)
+        #end
+      `),
+    })
+
+    datasource.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'changeStatus',
+      requestMappingTemplate: appsync.MappingTemplate.fromString(`
+        {
+          "version" : "2017-02-28",
+          "payload" : $util.toJson($context.arguments)
+        }
+      `),
+      responseMappingTemplate: appsync.MappingTemplate.fromString(`
+        #if($context.error)
+          $util.error($context.error.message, $context.error.type)
+        #else
+          $util.qr($context.result.put("status", "Processed : $context.result.status"))
+          $util.toJson($context.result)
+        #end
+      `),
+    })
   }
 }
