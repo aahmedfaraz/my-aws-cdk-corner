e9281193a3618ec02d91187298b066ac9a5579c9
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-11-25 00:40:34 +0500

    Complete - Stepp11 - Exp03 - CDK Backend
    Complete - Stepp11 - Exp03 - CDK Backend

--- step11_cognito/example03_google_login_cognito_amplify/cdk_cognito_google_login_backend/README.md
+++ step11_cognito/example03_google_login_cognito_amplify/cdk_cognito_google_login_backend/README.md
@@ -1,3 +1,29 @@
+# Steps
+
+1. Created UserPool.
+2. Created Google Developer Project.
+3. Created UserPool Google Identity Provider and Defined Google Client ID and Secret.
+4. Created UserPool Client to use at Client side.
+
+# Cognito Domain
+
+We also need to add a cognito domain which will serve up the login form. It is important to keep track of the domainPrefix that we set here because this is what we will be using in our google developer console. Which will have to be setup before we deploy this Cognito through CDK since it already requires the google console's client ids.
+
+In the dev console you will need to enter:
+
+- **Authorised JavaScript origins:**
+  - `https://DOMAIN-PREFIX.auth.eu-west-1.amazoncognito.com`
+- **Authorised redirect URIs:**
+  - `https://DOMAIN-PREFIX.auth.eu-west-1.amazoncognito.com/oauth2/idpresponse`
+
+```js
+const domain = userPool.addDomain("domain", {
+  cognitoDomain: {
+    domainPrefix: "DOMAIN-PREFIX",
+  },
+});
+```
+
 # Welcome to your CDK TypeScript project!
 
 This is a blank project for TypeScript development with CDK.
@@ -6,9 +32,9 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
 ## Useful commands
 
- * `npm run build`   compile typescript to js
- * `npm run watch`   watch for changes and compile
- * `npm run test`    perform the jest unit tests
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk synth`       emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `cdk deploy` deploy this stack to your default AWS account/region
+- `cdk diff` compare deployed stack with current state
+- `cdk synth` emits the synthesized CloudFormation template
--- step11_cognito/example03_google_login_cognito_amplify/cdk_cognito_google_login_backend/lib/cdk_cognito_google_login_backend-stack.ts
+++ step11_cognito/example03_google_login_cognito_amplify/cdk_cognito_google_login_backend/lib/cdk_cognito_google_login_backend-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as cognito from '@aws-cdk/aws-cognito';
+import { UserPoolEmail } from '@aws-cdk/aws-cognito';
 
 export class CdkCognitoGoogleLoginBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -7,9 +8,117 @@ export class CdkCognitoGoogleLoginBackendStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCognitoGoogleLoginBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create userpool
+    const userpool = new cognito.UserPool(this, 'myuserpool', {
+      // 
+      // SIGN UP using : *fullname, *email, *phone, *password. ( * means required )
+      // SIGN IN using : email, password
+      // VERIFYING     : email, phone
+      // 
+      userPoolName: 'userpool-for-google-and-facebook-login',
+      selfSignUpEnabled: true,
+      userVerification: {
+        // When a user signs up, email and SMS messages are used to verify their account and contact methods
+        emailSubject: 'Verify your email for our AhmedFarazApp',
+        emailBody: 'Thanks for signing in AhmedFarazApp, Your verification code is {####}',
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+        smsMessage: 'Thanks for signing in AhmedFarazApp, Your verification code is {####}',
+      },
+      userInvitation: {
+        // user then receives an invitation to join the user pool
+        emailSubject: 'Invite to join AhmedFarazApp',
+        emailBody: 'Hi {username}, you have been invited to join AhmedFarazApp! Your temporary password is {####}',
+        smsMessage: 'Hi {username}, you have been invited to join AhmedFarazApp! Your temporary password is {####}',
+      },
+      signInAliases: {
+        // user can sign in with their email address
+        email: true
+      },
+      autoVerify: {
+        email: true,
+        phone: true
+      },
+      standardAttributes: {
+        // defined attributes wanted for signup else custom attributes down below
+        fullname: {
+          required: true,
+          mutable: false
+        },
+        email: {
+          required: true,
+          mutable: false
+        },
+        phoneNumber: {
+          required: true,
+          mutable: true
+        }
+      },
+      // customAttributes: {
+        // 'ZipCode': new cognito.NumberAttribute({ min: 5, max: 5, mutable: true }),
+        // 'myappid': new cognito.StringAttribute({ minLen: 5, maxLen: 15, mutable: false }),
+        // 'isEmployee': new cognito.BooleanAttribute({ mutable: true }),
+        // 'joinedOn': new cognito.DateTimeAttribute(),
+      // },
+      passwordPolicy: {
+        minLength: 8,
+        requireDigits: true,
+        requireLowercase: true,
+        requireUppercase: true
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      email: UserPoolEmail.withCognito('ahmedfaraz.contact@gmail.com')
+      // email: UserPoolEmail.withSES({
+      //   sesRegion: 'us-west-2',
+      //   fromEmail: 'ahmedfaraz.contact@gmail.com',
+      //   fromName: 'Ahmed Faraz\'s Cognito App',
+      //   replyTo: 'ahmedfaraz.contact@gmail.com'
+      // })
+    })
+
+    // Create provider
+    const myGoogleProvider = new cognito.UserPoolIdentityProviderGoogle(this, 'myGoogleProvider', {
+      userPool: userpool,
+      clientId: '916101340968-n98gtsllo99hf62klutkdktd1r9judp8.apps.googleusercontent.com', // MY_GOOGLE_OAUTH_CLIENT_ID
+      clientSecret: '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', // MY_GOOGLE_OAUTH_CLIENT_SECRET
+      attributeMapping: {
+        fullname: cognito.ProviderAttribute.GOOGLE_GIVEN_NAME,
+        email: cognito.ProviderAttribute.GOOGLE_EMAIL,
+        phoneNumber: cognito.ProviderAttribute.GOOGLE_PHONE_NUMBERS,
+      },
+      scopes: ['profile', 'email', 'openid']
+    })
+
+    // connect provider and userpool
+    userpool.registerIdentityProvider(myGoogleProvider);
+
+    // Create userpool client to use at client side
+    const userpoolClient = new cognito.UserPoolClient(this, 'myClientForUserpoolWithGoogleProvider', {
+      userPool: userpool,
+      oAuth: {
+        callbackUrls: ['http://localhost:8000'] // This is the link where user will be redirected to with the code upon signin
+      }
+    })
+
+    // Define Domain when third party authentication
+    const domain = userpool.addDomain('theDomain', {
+      cognitoDomain: {
+        domainPrefix: 'ahmed-faraz-cog-app'
+      }
+    })
+
+    // Required Outputs
+    new cdk.CfnOutput(this, 'aws_user_pool_id', {
+      value: userpool.userPoolId
+    })
+    new cdk.CfnOutput(this, 'aws_user_pool_web_client_id', {
+      value: userpoolClient.userPoolClientId
+    })
+    new cdk.CfnOutput(this, 'aws_project_region', {
+      value: this.region
+    })
+    new cdk.CfnOutput(this, 'domain', {
+      value: domain.domainName
+    })
+  
   }
 }
