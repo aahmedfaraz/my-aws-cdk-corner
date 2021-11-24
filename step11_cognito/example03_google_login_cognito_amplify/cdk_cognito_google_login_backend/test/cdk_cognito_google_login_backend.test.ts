08004247759a06dadade48df99185153051c32c2
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-11-24 20:25:14 +0500

    Initialized - Step11 - Example03 - CDK Backend
    Initialized - Step11 - Example03 - CDK Backend
    - Backend to create login with third party (using here Login with Google)

---
+++ step11_cognito/example03_google_login_cognito_amplify/cdk_cognito_google_login_backend/test/cdk_cognito_google_login_backend.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as CdkCognitoGoogleLoginBackend from '../lib/cdk_cognito_google_login_backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_cognito_google_login_backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCognitoGoogleLoginBackend.CdkCognitoGoogleLoginBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
