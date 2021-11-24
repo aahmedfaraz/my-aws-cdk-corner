ca65ca88082455dc0ad1f99a98f34160433b9cdf
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-11-24 05:40:03 +0500

    Completed - Step11 - Example02 - CDK Cognito Userpool with Amplify Auth
    Completed - Step11 - Example02 - CDK Cognito Userpool with Amplify Auth

--- step11_cognito/example02_cognito_userpool_with_frontend_amplify/gatsby_amplify_frontend/src/pages/index.js
+++ step11_cognito/example02_cognito_userpool_with_frontend_amplify/gatsby_amplify_frontend/src/pages/index.js
@@ -1,30 +1,13 @@
 import React from "react";
 import { Authenticator } from '@aws-amplify/ui-react';
-
-// import { Auth } from 'aws-amplify';
+import '../styles/global.css';
 
 const Home = () => {
-    // const signup = async () => {
-    //     try {
-    //       const { user } = await Auth.signUp({
-    //           username: 'ahmedfarazNew2',
-    //           password: '1234567890Aa.',
-    //           attributes: {
-    //               email: 'ahmedfaraz838383@gmail.com',
-    //               phone_number: '+921234567'
-    //           }
-    //       });
-    //       console.log(user);
-    //     } catch (error) {
-    //         console.log('error signing up:', error);
-    //     }
-    // }
-    // signup();
   return (
     <Authenticator>
         {({ signOut, user }) => (
             <main>
-                <h1>Hello {user.username}</h1>
+                <h1>Hello <span>{user.username}</span></h1>
                 <button onClick={signOut}>Sign out</button>
             </main>
         )}
