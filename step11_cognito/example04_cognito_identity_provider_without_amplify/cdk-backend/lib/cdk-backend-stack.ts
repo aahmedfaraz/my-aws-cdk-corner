1b03da7c693629d6963820eff5e0037a7fe94120
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-11-27 00:56:33 +0500

    Completed CDK Backend - Step11 - Exp04 - Cognito Identity Provider without Amplify
    Completed CDK Backend - Step11 - Exp04 - Cognito Identity Provider without Amplify

--- step11_cognito/example04_cognito_identity_provider_without_amplify/cdk-backend/lib/cdk-backend-stack.ts
+++ step11_cognito/example04_cognito_identity_provider_without_amplify/cdk-backend/lib/cdk-backend-stack.ts
@@ -1,15 +1,67 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as cognito from '@aws-cdk/aws-cognito';
 
 export class CdkBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const userpool = new cognito.UserPool(this, 'myUserPoolForCogWithoutAmplify', {
+      selfSignUpEnabled: true,
+      userVerification: {
+        emailSubject: 'Verify your email for AhmedFaraz App',
+        emailBody: 'Hi! Thanks for Signing up, your verification code is {####}',
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+      },
+      signInAliases: {
+        username: true,
+        email: true,
+      },
+      autoVerify: {
+        email: true,
+      },
+      signInCaseSensitive: true,
+      standardAttributes: {
+        fullname: {
+          required: true,
+          mutable: true,
+        },
+        email: {
+          required: true,
+          mutable: false,
+        }
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+
+    const client = new cognito.UserPoolClient(this, 'myUserpoolClient', {
+      userPool: userpool,
+      generateSecret: true,
+      oAuth: {
+        flows: {
+          authorizationCodeGrant: true
+        },
+        scopes: [cognito.OAuthScope.OPENID, cognito.OAuthScope.EMAIL],
+        callbackUrls: [`http://localhost:8000/dashboard`],
+        logoutUrls: [`http://localhost:8000`]
+      }
+    });
+
+
+
+    const domain = userpool.addDomain('CognitoDomain', {
+      cognitoDomain: {
+        domainPrefix: 'app-without-amp'
+      }
+    });
+
+
+
+    const signInUrl = domain.signInUrl(client, {
+      redirectUri: `http://localhost:8000/dashboard`
+    });
+    
+
   }
 }
