179ef2acfeb65d0221f9cf98abd288eec815dc8d
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-11-29 02:23:54 +0500

    Initialized CDK App - Step 14 - Example02 - Pipeline update Frontend
    Initialized CDK App - Step 14 - Example02 - Pipeline update Frontend

---
+++ step14_CI_CD_pipeline/example02_CI_CD_pipeline_update_frontend/test/example02_ci_cd_pipeline_update_frontend.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Example02CiCdPipelineUpdateFrontend from '../lib/example02_ci_cd_pipeline_update_frontend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/example02_ci_cd_pipeline_update_frontend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Example02CiCdPipelineUpdateFrontend.Example02CiCdPipelineUpdateFrontendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
