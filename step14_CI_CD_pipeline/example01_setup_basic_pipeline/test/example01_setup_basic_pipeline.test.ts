91a2c5294da9ad64883a357fbbcf9da4f465b555
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-11-28 22:33:13 +0500

    Initialized CDK App - Step 14 - Example01 - Setup Basic CICD Pipeline
    Started Step14 - CI CD Pipeline
    - Initialized CDK App - Step 14 - Example01 - Setup Basic Pipeline

---
+++ step14_CI_CD_pipeline/example01_setup_basic_pipeline/test/example01_setup_basic_pipeline.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Example01SetupBasicPipeline from '../lib/example01_setup_basic_pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/example01_setup_basic_pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Example01SetupBasicPipeline.Example01SetupBasicPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
