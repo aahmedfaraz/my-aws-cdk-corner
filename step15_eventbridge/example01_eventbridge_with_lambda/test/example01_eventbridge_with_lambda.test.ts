26b67a9b92aa96fc6bdd64b0f58d162a42802d34
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-11-29 22:06:55 +0500

    Initialized CDK App - Step15 - Example 01 - EventBridge with Lambda
    Started Step15 - AWS EventBridge
    - Initialized CDK App - Step15 - Example 01 - EventBridge with Lambda

---
+++ step15_eventbridge/example01_eventbridge_with_lambda/test/example01_eventbridge_with_lambda.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Example01EventbridgeWithLambda from '../lib/example01_eventbridge_with_lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/example01_eventbridge_with_lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Example01EventbridgeWithLambda.Example01EventbridgeWithLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
