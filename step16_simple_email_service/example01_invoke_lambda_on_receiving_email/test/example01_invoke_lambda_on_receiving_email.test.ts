9b0e047d097c39614745901b90f8d8f8bd22e07c
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-12-10 23:20:11 +0500

    Initialized CDK App - Step16 : Exp01 - Invoke Lambda Function on Receiving Email
    Started Step16 - AWS SES (Simple Email Service)
    - Initialized CDK App - Step16 : Exp01
    - Invoke Lambda Function on Receiving Email

---
+++ step16_simple_email_service/example01_invoke_lambda_on_receiving_email/test/example01_invoke_lambda_on_receiving_email.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Example01InvokeLambdaOnReceivingEmail from '../lib/example01_invoke_lambda_on_receiving_email-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/example01_invoke_lambda_on_receiving_email-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Example01InvokeLambdaOnReceivingEmail.Example01InvokeLambdaOnReceivingEmailStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
