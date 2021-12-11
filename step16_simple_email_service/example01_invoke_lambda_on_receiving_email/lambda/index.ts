bbbbc9dcc32f484d00a4b7335d6f3cf6f8db3317
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-12-11 22:59:55 +0500

    Completed CDK App - Step16 : Exp01 - Invoke Lambda Function on Receiving Email
    Completed CDK App - Step16 : Exp01 - Invoke Lambda Function on Receiving Email

---
+++ step16_simple_email_service/example01_invoke_lambda_on_receiving_email/lambda/index.ts
@@ -0,0 +1,3 @@
+exports.handler = (event : any) => {
+    console.log('EVENT : ', JSON.stringify(event));
+}
--- step16_simple_email_service/example01_invoke_lambda_on_receiving_email/lib/example01_invoke_lambda_on_receiving_email-stack.ts
+++ step16_simple_email_service/example01_invoke_lambda_on_receiving_email/lib/example01_invoke_lambda_on_receiving_email-stack.ts
@@ -1,6 +1,8 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as ses from 'aws-cdk-lib/aws-ses';
+import * as sesActions from 'aws-cdk-lib/aws-ses-actions';
 
 export class Example01InvokeLambdaOnReceivingEmailStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -8,9 +10,31 @@ export class Example01InvokeLambdaOnReceivingEmailStack extends Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Example01InvokeLambdaOnReceivingEmailQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Setting a lambda function which will be run on receiving emails
+    const lambdaForReceivingEmails = new lambda.Function(this, 'lambdaForReceivingEmails', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'index.handler',
+    });
+
+
+    // Creating a new rule set
+    const ruleSet = new ses.ReceiptRuleSet(this, 'RuleSet', {
+      receiptRuleSetName: 'Rule-Set-Calling-Lambda',
+    })
+
+    
+    // Adding a rule inside the rule set
+    ruleSet.addRule('RULE_TO_INVOKE_LAMBDA', {
+      recipients: ['demo1@faraz.extechsolution.com'], // If no recipients than the action will be called on any incoming mail addresses of verified domains
+      actions: [
+        new sesActions.Lambda({ // defining an action to call when receive email on given recipients
+          function: lambdaForReceivingEmails,
+          invocationType: sesActions.LambdaInvocationType.EVENT
+        })
+      ],
+      scanEnabled: true, // Enable spam and virus scanning
+    })
+
   }
 }
