31c27ba50162271e45a9e14fb4f738d8027c2b69
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-12-12 22:53:37 +0500

    Initialize CDK App - Step16 : Exp03 - Send Emails using SES and Lambda
    Initialize CDK App - Step16 : Exp03 - Send Emails using SES and Lambda

---
+++ step16_simple_email_service/example03_send_emails_using_ses_and_lambda/test/example03_send_emails_using_ses_and_lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Example03SendEmailsUsingSesAndLambda from '../lib/example03_send_emails_using_ses_and_lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/example03_send_emails_using_ses_and_lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Example03SendEmailsUsingSesAndLambda.Example03SendEmailsUsingSesAndLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
