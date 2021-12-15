a78f6ce5c384a61681fcad15682e749715c1e142
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-12-15 16:30:05 +0500

    Initialize - Step23 Exp01 - Lambda with S3 Tracing
    Initialize - Step23 Exp01 - Lambda with S3 Tracing

---
+++ step23_x-ray/example01_lambda_with_s3_tracing/test/example01_lambda_with_s3_tracing.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Example01LambdaWithS3Tracing from '../lib/example01_lambda_with_s3_tracing-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/example01_lambda_with_s3_tracing-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Example01LambdaWithS3Tracing.Example01LambdaWithS3TracingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
