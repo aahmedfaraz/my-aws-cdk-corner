7c8afd1523d5336be02190755ac67ea8251528cf
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-12-15 13:20:20 +0500

    Initialized - Step23 Exp00 - Simple Lambda Tracing
    Initialized - Step23 Exp00 - Simple Lambda Tracing

---
+++ step23_x-ray/example00_simple_lambda_tracing/test/example00_simple_lambda_tracing.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Example00SimpleLambdaTracing from '../lib/example00_simple_lambda_tracing-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/example00_simple_lambda_tracing-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Example00SimpleLambdaTracing.Example00SimpleLambdaTracingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
