5855bd3efb1db841370428042e6ea72bf284da27
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-12-17 18:00:54 +0500

    Completed + Issue - Step23 Exp01 - Lambda with S3 Tracing
    Completed + Issue - Step23 Exp01 - Lambda with S3 Tracing
    - Issue: aws-xray-sdk-core is not supported anymore

--- step23_x-ray/example01_lambda_with_s3_tracing/lib/example01_lambda_with_s3_tracing-stack.ts
+++ step23_x-ray/example01_lambda_with_s3_tracing/lib/example01_lambda_with_s3_tracing-stack.ts
@@ -1,6 +1,7 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class Example01LambdaWithS3TracingStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -8,9 +9,32 @@ export class Example01LambdaWithS3TracingStack extends Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Example01LambdaWithS3TracingQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Creating a role for lambda function
+    const roleForLambda = new iam.Role(this, 'roleForLambda', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    })
+
+    // Creating S3 read only access policy to the role
+    const policyForS3ReadOnlyAccess = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ["s3:Get*", "s3:List*"],
+      resources: ['*']
+    })
+
+    // Granting iam policy to role
+    roleForLambda.addToPolicy(policyForS3ReadOnlyAccess);
+
+    // Creating a lambda
+    new lambda.Function(this, 'lambda-s3-x-ray-tracing', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'index.handler',
+      // Enabling x-ray tracicng
+      tracing: lambda.Tracing.ACTIVE,
+      role: roleForLambda,
+      timeout: Duration.minutes(1)
+    })
+
+
   }
 }
