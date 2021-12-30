6417fecae2f44da36c329bfe6391e6df07d2d614
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-12-30 21:43:45 +0500

    Completed S3 Glue Service with Python Job
    Completed S3 Glue Service with Python Job

---
+++ MY_EXPERIMENTS/exp_02_write_s3_using_glue_python_shell_job/test/exp_02_write_s3_using_glue_python_shell_job.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Exp02WriteS3UsingGluePythonShellJob from '../lib/exp_02_write_s3_using_glue_python_shell_job-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/exp_02_write_s3_using_glue_python_shell_job-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Exp02WriteS3UsingGluePythonShellJob.Exp02WriteS3UsingGluePythonShellJobStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
