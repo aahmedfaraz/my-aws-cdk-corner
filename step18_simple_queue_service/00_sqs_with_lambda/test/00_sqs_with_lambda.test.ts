11cf7d16242e8cc626798f4add4ca03bc039eee3
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2022-02-20 00:29:46 +0500

    Initialized CDK App
    Initialized CDK App

---
+++ step18_simple_queue_service/00_sqs_with_lambda/test/00_sqs_with_lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as 00SqsWithLambda from '../lib/00_sqs_with_lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/00_sqs_with_lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new 00SqsWithLambda.00SqsWithLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
